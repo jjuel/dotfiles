5b8cae8f1c4ba645cad17f2ea2acd4007dfabcf2
Jordan Juel <Jordan.Juel@ameritas.com>
2020-05-07 13:43:16 -0500

    Adding Fish and other minor changes for Emacs and Tmux

---
+++ .config/fish/config.fish
@@ -0,0 +1,44 @@
+if not set -q __fish_prompt_hostname
+	set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+end
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showuntrackedfiles 'no'
+set __fish_git_prompt_showupstream 'no'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+
+function fish_prompt
+	set last_status $status
+
+	set_color red 
+	printf "["
+	set_color yellow
+	printf "%s" "$USER"
+	set_color green
+	printf "@"
+	set_color blue
+	printf "%s" "$__fish_prompt_hostname"
+	set_color magenta
+	printf " %s" "$PWD"
+	set_color red
+	printf "] "
+	set_color normal
+
+	printf "%s" (__fish_git_prompt)
+	set_color normal
+
+	printf "%s\n" ""
+	set_color yellow
+	printf "%s" "❯"
+	set_color blue
+	printf "%s" "❯"
+	set_color magenta
+	printf "%s " "❯"
+	set_color normal
+end
